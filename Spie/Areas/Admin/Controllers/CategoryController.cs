0a807b2633473703494c9122b762cb640838393d
KujawskiSzymon <42217160+KujawskiSzymon@users.noreply.github.com>
2019-12-12 20:43:54 +0100

    Category Model; Category Controller(Admin)

---
+++ Spie/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spie.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+       private readonly ApplicationDbContext dbContext; 
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            dbContext = db;
+        }
+        public async Task< IActionResult> Index()
+        {
+
+            return  View(await dbContext.Category.ToListAsync());
+        }
+    }
+}
