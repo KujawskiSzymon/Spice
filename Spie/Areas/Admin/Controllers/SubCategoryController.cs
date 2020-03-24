fbc3c9ea953bfd61a85eafa1782cc7867fb985ac
KujawskiSzymon <42217160+KujawskiSzymon@users.noreply.github.com>
2020-03-24 17:56:40 +0100

    Sub Category - migration, get index

---
+++ Spie/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spie.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            dbContext = db;
+        }
+
+        //GET INDEX
+        public async Task<IActionResult> Index()
+        {
+            var SubCategories = await dbContext.SubCategory.Include(cat=>cat.Category).ToListAsync();
+            return View(SubCategories);
+        }
+
+       
+    }
+}
