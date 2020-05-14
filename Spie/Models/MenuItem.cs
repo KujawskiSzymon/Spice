a436beb89083c9fa785788dad727776f814e4cf8
KujawskiSzymon <42217160+KujawskiSzymon@users.noreply.github.com>
2020-05-14 18:16:41 +0200

    MenuItem model

---
+++ Spie/Models/MenuItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class MenuItem
+    {
+        public int Id{ get; set; }
+        public string Name{ get; set; }
+        public string Description{ get; set; }
+
+        public string Spicyness { get; set; }
+
+        public enum ESpicy { NA,NotSpicy,Spicy,VerySpicy}
+        public string Image { get; set; }
+        [Display(Name="Category")]
+        public int CategoryId{ get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+
+
+        [Display(Name = "Sub Category")]
+        public int SubCategoryId{ get; set; }
+
+        [ForeignKey("SubCategoryId")]
+        public virtual SubCategory SubCategory { get; set; }
+
+        [Range(1,int.MaxValue,ErrorMessage ="Price Should Be Greater Than ${1}")]
+        public double Price{ get; set; }
+    }
+}
